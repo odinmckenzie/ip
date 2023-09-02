9ff209e454ecd5467b4aaaac9b2c855d7dd70649
Odin McKenzie <odin.mckenzie@gmail.com>
2023-09-01 23:47:49 -0400

    Add `add` method limit tests

--- tests/IPv4AddressTest.php
+++ tests/IPv4AddressTest.php
@@ -19,22 +19,23 @@ class IPv4AddressTest extends TestCase
     /**
      * @dataProvider invalidIPv4AddressProvider
      */
-    public function testInvalidIPv4AddressInput($ip, $code)
+    public function testInvalidIPv4AddressInput($ip, $message)
     {
         $this->expectException(InvalidAddressException::class);
-        $this->expectExceptionCode($code);
+        $this->expectExceptionMessage($message);
         new IPv4Address($ip);
     }
 
     public function invalidIPv4AddressProvider()
     {
         return [
-            ['192.168.1.256', 0],
-            ['192.168.1.1.1', 0],
-            ['192.168.1.', 0],
-            ['192.168', 0],
-            ['192.168.1.1/24', 1],
-            ['192.168.1.1/255.255.255.0', 1],
+            ['192.168.1.256', "'192.168.1.256' is in an unexpected format"],
+            ['192.168.1.1.1', "'192.168.1.1.1' is in an unexpected format"],
+            ['192.168.1.', "'192.168.1.' is in an unexpected format"],
+            ['192.168', "'192.168' is in an unexpected format"],
+
+            ['192.168.1.1/24', "Unexpected '/' found in '192.168.1.1/24'"],
+            ['192.168.1.1/255.255.255.0', "Unexpected '/' found in '192.168.1.1/255.255.255.0'"],
         ];
     }
 
@@ -57,5 +58,23 @@ class IPv4AddressTest extends TestCase
         $next_ip = $ip->add(1);
 
         $this->assertEquals('192.168.1.2', $next_ip->address());
+    }
+
+    public function testIPv4AddressAddUpperLimit()
+    {
+        $this->expectException(InvalidAddressException::class);
+        $this->expectExceptionMessage("'255.255.255.250' + 6 is greater than '255.255.255.255'");
+
+        $ip = new IPv4Address('255.255.255.250');
+        $ip->add(6);
+    }
+
+    public function testIPv4AddressAddLowerLimit()
+    {
+        $this->expectException(InvalidAddressException::class);
+        $this->expectExceptionMessage("'0.0.0.0' + -2 is less than '0.0.0.0'");
+
+        $ip = new IPv4Address('0.0.0.0');
+        $ip->add(-2);
     }
 }
