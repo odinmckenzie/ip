534a57bfbc3f2d9de832441bcd268b5152e8a804
Odin McKenzie <odin.mckenzie@gmail.com>
2023-09-03 22:11:12 -0400

    Add constructor test

---
+++ tests/IPv4NetworkTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Odin\IP\IPv4Network;
+
+class IPv4NetworkTest extends TestCase
+{
+    public function testSimpleObjCreate()
+    {
+        $net = new IPv4Network('192.168.1.1', 24);
+        $this->assertEquals('192.168.1.0', $net->address());
+    }
+}
