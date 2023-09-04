d9caff34ccffc000f056bd7dde908a16a7b1fab2
Odin McKenzie <odin.mckenzie@gmail.com>
2023-09-03 23:23:52 -0400

    Add firstIP test

--- tests/IPv4NetworkTest.php
+++ tests/IPv4NetworkTest.php
@@ -46,6 +46,13 @@ class IPv4NetworkTest extends TestCase
         $this->assertEquals(new IPv4Address('192.168.1.255'), $net->broadcast());
     }
 
+    public function testFirstIP()
+    {
+        $net = new IPv4Network('192.168.1.0', 24);
+        
+        $this->assertEquals(new IPv4Address('192.168.1.1'), $net->firstIP());
+    }
+
     public function testContains()
     {
         $net = new IPv4Network('192.168.1.0', 30);
